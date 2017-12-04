83e60996e8831c0ec2f407f3985fb7d87579f2bf
Mikael Airlangga Trianggoro <mikael.airlangga@gmail.com>
2017-12-04 23:36:07 +0800

    4-46: Creating an Action

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,3 +1,8 @@
 export function selectBook(book) {
-  console.log('A Book has been selected ', book.title);
+  // selectBook is an action creator, it needs to return an action, 
+  // an object with a type property, and with a payload.
+  return {
+    type: 'BOOK_SELECTED',
+    payload: book
+  }
 }
