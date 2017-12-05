6fe1fdde5794c2e67b6e80aeb1e7d9c1cdd5f056
Mikael Airlangga Trianggoro <mikael.airlangga@gmail.com>
2017-12-05 16:01:00 +0800

    4-49: Conditional Rendering

--- src/reducers/reducer_books.js
+++ src/reducers/reducer_books.js
@@ -1,8 +1,8 @@
 export default function () {
   return [
-    { title: "Javascript: The Good Parts" },
-    { title: "Harry Potter: The Sorcerer's Stone" },
-    { title: "The Dark Tower" },
-    { title: "The Eloquent Ruby" }
+    { title: "Javascript: The Good Parts", pages: 145 },
+    { title: "Harry Potter: The Sorcerer's Stone", pages: 78 },
+    { title: "The Dark Tower", pages: 65 },
+    { title: "The Eloquent Ruby", pages: 1 }
   ]
 }
